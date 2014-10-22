6be21820b6e0122d7b6b819aefcbcae9aaf3cd75
jameslimpiado <limpiado_james@yahoo.com>
2014-10-22 10:53:57 +0800

    Add guard, include bootstrap and fontawesome, add main slim pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'site/home'
+  get 'site/about'
+  get 'site/founders'
+  get 'site/reviewers'
+  get 'site/pricing'
+
+  root 'site#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
