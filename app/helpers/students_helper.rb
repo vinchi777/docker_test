8a4da61e8cf4d52330164ca5600b81fef03c0770
jkdeveyra <jkdeveyra@gmail.com>
2014-10-27 18:59:47 +0800

    Finalize implementation of student mockup

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,24 @@
 module StudentsHelper
+
+  def enrollment_status(student)
+    res =
+        if student[:status][:name] == 'Enrolled'
+          '<i class="fa fa-lg fa-check"></i> ' + student[:status][:name] + ' ' + student[:status][:season]
+        elsif student[:status][:name] == 'Enrolling'
+          '<i class="fa fa-lg fa-clock-o"></i> ' + student[:status][:name] + ' for ' + student[:status][:season]
+        end
+
+    raw(res)
+  end
+
+  def payment_status(student)
+    res =
+        if student[:balance].nil?
+          ''
+        else
+          '<i class="fa fa-lg fa-money"></i>' +
+              number_to_currency(student[:balance], unit: 'Php', format: '%u %n')
+        end
+    raw(res)
+  end
 end
