7a9bebef56960bff6090ea4156fe687f8245f908
jkdeveyra <jkdeveyra@gmail.com>
2014-10-28 10:46:04 +0800

    Disable BetterErrors for vagrant

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,15 +34,15 @@ Rails.application.configure do
 
   # Allow better errors to access vagrant
   # Install plugin first -> vagrant plugin install vagrant-host-path
-  if defined? BetterErrors
-    # Opening files
-    BetterErrors.editor = proc { |full_path, line|
-      full_path = full_path.sub(Rails.root.to_s, ENV["VAGRANT_HOST_PATH"])
-      "subl://open?url=file://#{full_path}&line=#{line}"
-    }
-
-    # Allowing host
-    host = ENV["SSH_CLIENT"] ? ENV["SSH_CLIENT"].match(/\A([^\s]*)/)[1] : nil
-    BetterErrors::Middleware.allow_ip! host if [:development, :test].member?(Rails.env.to_sym) && host
-  end
+  # if defined? BetterErrors
+  #   # Opening files
+  #   BetterErrors.editor = proc { |full_path, line|
+  #     full_path = full_path.sub(Rails.root.to_s, ENV["VAGRANT_HOST_PATH"])
+  #     "subl://open?url=file://#{full_path}&line=#{line}"
+  #   }
+  #
+  #   # Allowing host
+  #   host = ENV["SSH_CLIENT"] ? ENV["SSH_CLIENT"].match(/\A([^\s]*)/)[1] : nil
+  #   BetterErrors::Middleware.allow_ip! host if [:development, :test].member?(Rails.env.to_sym) && host
+  # end
 end
