c529a03c156be28778a5619e16d7560bff4008a9
jameslimpiado <limpiado_james@yahoo.com>
2014-11-03 13:45:45 +0800

    Add error handling in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,33 @@ var path = require('path');
 var rename = require('gulp-rename');
 var changed = require('gulp-changed');
 var tap = require('gulp-tap');
+var gutil = require('gulp-util')
 
 var less_files = 'app/assets/stylesheets/less/*.less*'
 var dest = 'app/assets/stylesheets'
+
+function do_less(f_path){
+   return gulp.src(f_path)
+        .pipe(less())
+        .on('error',gutil.log)
+
+}
 
 gulp.task('less', function () {
   gulp.src(less_files)
       .pipe(changed(dest))
       .pipe(tap(function (file, t) {
-        if (path.extname(file.path) === '.erb') {
-          return gulp.src(file.path)
-              .pipe(less())
-              .pipe(rename(path.basename(file.path, '.less.erb') + '.css.erb'))
+        f_path = file.path
+        if (path.extname(f_path) === '.erb')
+          return do_less(f_path)
+              .pipe(rename(path.basename(f_path, '.less.erb') + '.css.erb'))
               .pipe(gulp.dest(dest));
-        } else {
-          return gulp.src(file.path)
-              .pipe(less())
+        else
+          return do_less(f_path)
               .pipe(gulp.dest(dest));
-        }
       }))
 });
+
 
 gulp.task('watch', function () {
   gulp.watch(less_files, ['less']);
