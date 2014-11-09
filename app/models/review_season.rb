b3f2b582ccddfeaa04db1a29f65ef87a072c8f4a
jkdeveyra <jkdeveyra@gmail.com>
2014-11-10 00:32:04 +0800

    Fix display of amount in review seasons page

--- app/models/review_season.rb
+++ app/models/review_season.rb
@@ -16,19 +16,26 @@ class ReviewSeason
   field :promo_end, type: Date
 
   field :first_timer, type: BigDecimal
+  validates_numericality_of :first_timer, less_than: 1000000, message: 'must be less than 999,999.00'
+
   field :repeater, type: BigDecimal
+  validates_numericality_of :repeater, less_than: 1000000, message: 'must be less than 999,999.00'
 
   field :full_review, type: BigDecimal
   validates_presence_of :full_review
+  validates_numericality_of :full_review, less_than: 1000000, message: 'must be less than 999,999.00'
 
   field :double_review, type: BigDecimal
   validates_presence_of :double_review
+  validates_numericality_of :double_review, less_than: 1000000, message: 'must be less than 999,999.00'
 
   field :coaching, type: BigDecimal
   validates_presence_of :coaching
+  validates_numericality_of :coaching, less_than: 1000000, message: 'must be less than 999,999.00'
 
   field :reservation, type: BigDecimal
   validates_presence_of :reservation
+  validates_numericality_of :reservation, less_than: 1000000, message: 'must be less than 999,999.00'
 
   validate :date_precedence
 
