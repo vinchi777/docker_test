5d43559ab8748fc662e9de56bb9c06eee40c05d1
jkdeveyra <jkdeveyra@gmail.com>
2014-11-23 16:03:39 +0800

    Create first scenario for adding student payment

--- features/step_definitions/student_payment_step.rb
+++ features/step_definitions/student_payment_step.rb
@@ -2,22 +2,60 @@ And /^I select a student$/ do
   if Student.exists?
     @student = Student.first
   else
+    @student = Student.create(
+        firstName: 'John',
+        lastName: 'dela Cruz',
+        sex: 'Male',
+        address: 'Tacloban City',
+        contactNo: '321-444',
+        email: 'jdelacruz@gmail.com',
+        lastAttended: 'Cebu Institute of Medicine',
+        yearGrad: 2014,
+        hs: 'St. Marys Academy',
+        hsYear: 2006,
+        elem: 'Luntad Elem. School',
+        elemYear: 2002
+    )
   end
-  pending # express the regexp above with the code you wish you had
+  @student.save
+
+  ReviewSeason.create!(
+      season: 'May 2014',
+      first_timer: 0,
+      repeater: 0,
+      season_start: 'Apr. 1, 2014',
+      season_end: 'Apr. 10, 2014',
+      full_review: 17000,
+      double_review: 22000,
+      coaching: 7000,
+      reservation: 3000
+  )
+  visit student_path(@student)
 end
 
 When /^I add a student invoice$/ do
-  pending # express the regexp above with the code you wish you had
+  visit student_invoices_path(id: @student.id)
+  find('.new-invoice.btn').click
 end
 
 When /^I fill up these invoice information$/ do |table|
-  pending # express the regexp above with the code you wish you had
+  table.raw.each do |name, value, type|
+    case type
+      when 'text'
+        fill_in name, with: value
+      when 'select'
+        select value, from: name
+    end
+  end
 end
 
 And /^I submit the invoice form$/ do
-  pending # express the regexp above with the code you wish you had
+  click_on 'Add Invoice'
 end
 
-Then /^I should see the invoice on the student payment form$/ do
-  pending # express the regexp above with the code you wish you had
+Then /^I should see these invoice information on the student invoice form$/ do |table|
+  expect(all('.modal').count).to eq 0
+  table.raw.each do |text|
+    expect(page).to have_content text[0]
+  end
 end
--- features/students/add_student_payment.feature
+++ features/students/add_student_payment.feature
@@ -5,7 +5,13 @@ Feature: Add student payment
     And I select a student
     When I add a student invoice
     And I fill up these invoice information
+      | invoice[package] | Full Package | text   |
+      | invoice[season]  | May 2014     | select |
+      | invoice[amount]  | 17000        | text   |
     And I submit the invoice form
-    Then I should see the invoice on the student payment form
+    Then I should see these invoice information on the student invoice form
+      | Full Package |
+      | May 2014     |
+      | 17,000       |
 
   Scenario: Insert all fields
