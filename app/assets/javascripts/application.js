0095c381f02ba4a9d04d7e56763563afbbcb31fa
sAnti_09 <limpiado_james@yahoo.com>
2014-11-30 00:01:39 +0800

    Add emblemjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,8 +25,9 @@
 //= require math
 //= require auto_compute
 //= require svg-injector
-//= require handlebars-v1.3.0
 //= require ember-1.8.1
+//= require emblem
+//= require handlebars-v1.3.0
 //= require_self
 //= require gmaps
 //= require_tree .
