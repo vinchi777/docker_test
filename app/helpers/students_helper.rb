f7b20f1784fb9e15116f4d57ae8438ba13e3cbcf
jameslimpiado <limpiado_james@yahoo.com>
2014-12-09 20:00:40 +0800

    Change merge grade and test implementation from concat to + to prevent mutation

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -8,7 +8,7 @@ module StudentsHelper
   end
 
   def grades_in_enrollment(enrollment)
-    grades = enrollment.student_grades.concat enrollment.answer_sheets.select { |s| s.deadline? }
-    enrollment.student_grades.sort { |a, b| b.date <=> a.date }
+    grades = enrollment.student_grades + enrollment.answer_sheets.select { |s| s.deadline? }
+    grades.sort { |a, b| b.date <=> a.date }
   end
 end
