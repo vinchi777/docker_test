107e21503d687f54969f885c109bda4d02ad8b47
jameslimpiado <limpiado_james@yahoo.com>
2014-12-17 16:42:31 +0800

    Catch scenario if there is no ongoing review season in header, pricing and enrollment

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -20,6 +20,16 @@ class SiteController < ApplicationController
 
   def pricing
     @season = ReviewSeason.current
+    unless @season
+      @season = ReviewSeason.new({
+                                     first_timer: 14000,
+                                     repeater: 12000,
+                                     full_review: 16000,
+                                     double_review: 22000,
+                                     coaching: 7000,
+                                     reservation: 3000,
+                                 })
+    end
   end
 
   def our_students
